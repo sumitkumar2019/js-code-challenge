a60bcc6059a12a776a0638495b991df7c2cfeaf5
sky <sky@localhost.localdomain>
2020-08-21 20:53:21 -0400

    Test solutions added

--- src/datasource_connector_tool/solution.js
+++ src/datasource_connector_tool/solution.js
@@ -1,3 +1,48 @@
-// Problem 2
-// Task: Implement a datasource connector to abstract away data retrieval and manipulation from the `ViewControllers`.  
-// Your solution shall use only [Vanilla JavaScript](http://vanilla-js.com).  
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+class Datasource {
+    constructor() {}
+    async getResponse() {
+            var promiseObj = new Promise(function(resolve, reject){
+               var xhr = new XMLHttpRequest();
+               xhr.open("GET", "https://static.ngnrs.io/test/prices", true);
+               xhr.send();
+               xhr.onreadystatechange = function(){
+               if (xhr.readyState === 4){
+                  if (xhr.status === 200){
+                     var resp = xhr.responseText;
+                     var respJson = JSON.parse(resp);
+                     resolve(respJson.data.prices);
+                  } else {
+                     reject(xhr.status);
+                  }
+               } 
+            }
+          });
+          return promiseObj;
+         }
+         mid(){
+            return (this.buy+this.sell)/200;
+         }
+
+         quote(){
+            return this.pair.slice(-3)
+         }
+         async getPrices() {
+            const prices = await this.getResponse();
+            prices.forEach(price=>{
+                price.mid=this.mid.bind(price);
+                price.quote = this.quote.bind(price);
+            })
+            return prices
+         }
+
+   }
+let ds = new Datasource();
+ds.getPrices()
+    .then(prices => {
+        prices.forEach(price => {
+            console.log(`Mid price for ${ price.pair } is ${ price.mid() } ${ price.quote() }.`);
+        });
+    }).catch(error => {
+        console.error(error);
+    });
--- src/three_ways_to_sum_to_n/solution_a.js
+++ src/three_ways_to_sum_to_n/solution_a.js
@@ -1,8 +1,6 @@
-// Problem 1 A
-// Task: Provide 3 unique implementations of the following function.
-// Input: `n` - any integer from `0` to `Number.MAX_SAFE_INTEGER`.
-// Output: `return` - summation to `n`, i.e. sum_to_n(5) === 1 + 2 + 3 + 4 + 5 === 15.
-
-var sum_to_n = function(n) {
-    // your code here
-};
+sum_to_N= (N) =>{
+    let sum = 0;
+    for(let i=1; i<=N; i++)sum+=i;
+    return sum
+}
+console.log(sum_to_N(6))
